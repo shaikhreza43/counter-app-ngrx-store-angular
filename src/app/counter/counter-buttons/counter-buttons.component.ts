be4800829d2c325055c773d91ec43686d15e1ad5
shaikhreza43 <reza.rk786@gmaail.com>
2023-06-26 16:33:26 +0530

    code refractoring

--- src/app/counter/counter-buttons/counter-buttons.component.ts
+++ src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { decrement, increment, reset } from '../state/counter.action';
+import { CounterState } from '../state/counter.state';
 
 @Component({
   selector: 'app-counter-buttons',
@@ -8,7 +9,7 @@ import { decrement, increment, reset } from '../state/counter.action';
   styleUrls: ['./counter-buttons.component.css'],
 })
 export class CounterButtonsComponent {
-  constructor(private store: Store<{ counter: { counter: number } }>) {}
+  constructor(private store: Store<{ counter: CounterState }>) {}
 
   onIncrement() {
     this.store.dispatch(increment());
--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { CounterState } from '../state/counter.state';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css'],
 })
-export class CounterOutputComponent implements OnInit {
-  counter: number = 0;
+export class CounterOutputComponent implements OnInit, OnDestroy {
+  // counter: number = 0;
+  //counterSubscription: Subscription = new Subscription;//Not Required if we use variable of type Observable with $
 
-  constructor(private store: Store<{ counter: { counter: number } }>) {}
+  counter$?: Observable<CounterState>;
+
+  constructor(private store: Store<{ counter:CounterState }>) {}
+
+  ngOnDestroy(): void {
+    //  if(this.counterSubscription){
+    //   this.counterSubscription.unsubscribe();
+    //  }
+  }
 
   ngOnInit() {
-    this.store.select('counter').subscribe((res) => {
-      this.counter = res.counter;
-    });
+    // this.counterSubscription = this.store.select('counter').subscribe((res) => {
+    //   this.counter = res.counter;
+    // });
+
+    //OR
+
+    this.counter$ = this.store.select('counter');
   }
 }
